24f3645bf3fbfff4613874e1a324ad44ed7c8fa5
yrang <yangxu@app-lab.cn>
2017-09-21 12:50:46 +0800

    edit user fix1

--- src/app/powers/setuser/setuserPageCtrl.js
+++ src/app/powers/setuser/setuserPageCtrl.js
@@ -3,9 +3,8 @@
 
     angular.module('BlurAdmin.power.setuser')
         .controller('setuserPageCtrl', setuserPageCtrl)
-        .controller('addUserCtrl', addUserCtrl);
-
-    // .controller('editUserCtrl', editUserCtrl);
+        .controller('addUserCtrl', addUserCtrl)
+        .controller('editUserCtrl', editUserCtrl);
 
     /** @ngInject */
     function setuserPageCtrl($scope, PageTopCache, Log, User, UserHelper, HttpToast, ExportPrefix,
@@ -74,7 +73,118 @@
 
     }
 
-    function addUserCtrl($scope, Keyword, KeywordCache, HttpToast, ToastUtils, User, UserHelper, treeCache, kCache) {
+    function addUserCtrl($scope, treeCache, kCache, User, UserHelper, HttpToast, ToastUtils) {
+
+        $scope.show = {
+            userContracttyptArr: [],//合同类型
+            userEducationArr: [],  //教育程度
+            userAuthorityArr: [],  //权限
+            userStatusArr: [],  //状态
+            sidebarArr: [], //变电站数组
+
+            contractType: '',   //合同类型
+            education: '',   //教育程度
+            authority: '',   //权限
+            status: '',   //状态
+            clientName: '', //所属变电站
+            msgLevel: false, //是否接收短信
+            isMustArr: ['code', 'position', 'tipLine', 'contractType', 'education']   // 选填的数组
+        };
+
+        $scope.form = {
+            name: '',   //姓名
+            code: '',   //工号
+            position: '',   //岗位
+            client_ids: [],    //所辖变电站
+            tipLine: '',    //内线
+            authority: '',  //权限
+            status: '',     //状态
+
+            contractType: '',   //合同类型
+            education: '',      //教育程度
+            phone1: '',         //手机1
+            phone2: '',         //手机2 非必填
+            msgLevel: '',   //是否接收短信
+
+            account: '',        //平台账号
+            psw: ''             //密码
+        };
+
+        $scope.init = function () {
+
+            var pm = treeCache.getTree();
+            pm.then(function (data) {
+                $scope.show.sidebarArr = treeCache.createClientArr(data);
+            });
+
+            var pmKey = kCache.getKey();
+            pmKey.then(function (data) {
+                $scope.show.userContracttyptArr = kCache.getUser_contracttypt(data);
+                $scope.show.userEducationArr = kCache.getUser_education(data);
+                $scope.show.userAuthorityArr = kCache.getUser_authority(data);
+                $scope.show.userStatusArr = kCache.getUser_status();
+            });
+
+        };
+        $scope.init();
+
+        $scope.submit = function () {
+            $scope.show.msgLevel ? $scope.form.msgLevel = '1' : $scope.form.msgLevel = '0';
+
+            var p = UserHelper.add($scope.show.isMustArr, $scope.form);
+
+
+            if (p) {
+                User.create(p,
+                    function (data) {
+                        ToastUtils.openToast('success', data.message);
+                        $scope.$close(data);
+                    },
+                    function (err) {
+                        HttpToast.toast(err);
+                    })
+            }
+        };
+
+        // dropdown set
+        $scope.setContracttype = function (obj) {
+            $scope.show.contractType = obj.name;
+            $scope.form.contractType = obj.id;
+        };
+        $scope.setEducation = function (obj) {
+            $scope.show.education = obj.name;
+            $scope.form.education = obj.id;
+        };
+        $scope.setAuthority = function (obj) {
+            $scope.show.authority = obj.name;
+            $scope.form.authority = obj.id;
+        };
+        $scope.setStatus = function (obj) {
+            $scope.show.status = obj.name;
+            $scope.form.status = obj.id;
+        };
+        $scope.changeClent = function (obj) {
+            obj.state = !obj.state;
+
+            /**
+             * 遍历变电站数组，看那个是选中的
+             */
+            $scope.show.clientName = '';
+            $scope.form.client_ids = [];
+            $scope.show.sidebarArr.map(function (item) {
+                if (item.state) {
+                    $scope.show.clientName += item.clientName + "，";
+                    $scope.form.client_ids.push(item.clientId);
+                }
+            });
+            if ($scope.show.clientName.length > 0) {
+                $scope.show.clientName = $scope.show.clientName.substring(0, $scope.show.clientName.length - 1);
+            }
+
+        }
+    }
+
+    function editUserCtrl($scope, treeCache, kCache, User, UserHelper, HttpToast, ToastUtils) {
 
         $scope.show = {
             userContracttyptArr: [],//合同类型
